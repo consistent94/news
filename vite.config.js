ae5e67ebf89827cd309f0b3e1cce7065545ee4b2
Sebastian  Wilczega <sebastianwilczega@sebastians-mbp.home>
2024-09-07 14:26:43 +0100

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,13 @@
 import { defineConfig } from 'vite';
+import ejs from 'vite-plugin-ejs';
 
 export default defineConfig({
-  root: 'views'
+  root: 'views', 
+  plugins: [ejs()],
+  build: {
+    outDir: '../dist', // Specify the output directory
+    rollupOptions: {
+      input: 'views/index.ejs' // Set the entry point to your EJS file
+    }
+  }
 });
